d35f3d822867ed114bcfd8181f97de3b4ec37772
Ted John <ted@brambles.org>
2018-12-30 02:05:29 +0000

    Rename methods

--- src/openrct2/config/Config.cpp
+++ src/openrct2/config/Config.cpp
@@ -473,7 +473,7 @@ namespace Config
         }
     }
 
-    static void WriteNotifications(IIniWriter* writer)
+    static void ReadWriteNotificationsChunk(IIniWriter* writer)
     {
         auto model = &gConfigNotifications;
         writer->WriteSection("notifications");
@@ -611,7 +611,7 @@ namespace Config
             WriteInterface(writer.get());
             WriteSound(writer.get());
             WriteNetwork(writer.get());
-            WriteNotifications(writer.get());
+            ReadWriteNotificationsChunk(writer.get());
             WriteFont(writer.get());
             WritePlugin(writer.get());
             return true;
