ce0150f1643d2bead95c2198ff23e338a5f29f78
spacek531 <spacek531@comcast.net>
2021-01-13 23:18:49 -0800

    partial implementation block_brakes_set_linked_brakes_open todo
    mostly completed implementation
    
    set block brake speed to 4 when building
    
    fix import/export comments
    
    remove unecessary variable
    
    satisfy warnings
    
    fix fallthrough
    
    unify brake/block brake closed status part 1
    
    rename action functions
    
    fix import and export and others
    
    fix desync
    
    fix formatting
    
    remove seat rotation from block brakes
    
    update brake status when changing brake speed
    
    fix format
    
    set bbrake speed on TD6 import
    
    fix rebase error
    
    revert unnecessary enum changes
    
    move functions and declarations to better places
    
    move open brake condition into velocity condition
    
    move Track.cpp functions to a better place
    
    revert polarity of SetBrakeClosed
    
    fix rebase error
    
    revert changes to strings

--- src/openrct2-ui/windows/TileInspector.cpp
+++ src/openrct2-ui/windows/TileInspector.cpp
@@ -882,7 +882,7 @@ static void window_tile_inspector_mouseup(rct_window* w, rct_widgetindex widgetI
                 }
                 case WIDX_TRACK_CHECK_BLOCK_BRAKE_CLOSED:
                     window_tile_inspector_track_set_block_brake(
-                        windowTileInspectorSelectedIndex, !tileElement->AsTrack()->BlockBrakeClosed());
+                        windowTileInspectorSelectedIndex, !tileElement->AsTrack()->GetBrakeClosed());
                     break;
                 case WIDX_TRACK_CHECK_IS_INDESTRUCTIBLE:
                     window_tile_inspector_track_set_indestructible(
@@ -1557,7 +1557,7 @@ static void window_tile_inspector_invalidate(rct_window* w)
             w->widgets[WIDX_TRACK_CHECK_IS_INDESTRUCTIBLE].bottom = GBBB(propertiesAnchor, 4);
             WidgetSetCheckboxValue(w, WIDX_TRACK_CHECK_APPLY_TO_ALL, windowTileInspectorApplyToAll);
             WidgetSetCheckboxValue(w, WIDX_TRACK_CHECK_CHAIN_LIFT, tileElement->AsTrack()->HasChain());
-            WidgetSetCheckboxValue(w, WIDX_TRACK_CHECK_BLOCK_BRAKE_CLOSED, tileElement->AsTrack()->BlockBrakeClosed());
+            WidgetSetCheckboxValue(w, WIDX_TRACK_CHECK_BLOCK_BRAKE_CLOSED, tileElement->AsTrack()->GetBrakeClosed());
             WidgetSetCheckboxValue(w, WIDX_TRACK_CHECK_IS_INDESTRUCTIBLE, tileElement->AsTrack()->IsIndestructible());
             break;
         case TileInspectorPage::Scenery:
