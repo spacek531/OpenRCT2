8ad23d44ca45cd054cd5b18f05f72ff439a30929
spacek531 <spacek531@comcast.net>
2021-01-13 23:18:49 -0800

    partial implementation block_brakes_set_linked_brakes_open todo

--- src/openrct2/rct2/S6Importer.cpp
+++ src/openrct2/rct2/S6Importer.cpp
@@ -1155,6 +1155,7 @@ public:
                 dst2->SetInverted(src2->IsInverted());
                 dst2->SetStationIndex(src2->GetStationIndex());
                 dst2->SetHasGreenLight(src2->HasGreenLight());
+                //dst2->SetBrakeOpen(src2->BrakeOpen());
                 dst2->SetBlockBrakeClosed(src2->BlockBrakeClosed());
                 dst2->SetIsIndestructible(src2->IsIndestructible());
                 // Skipping IsHighlighted()
