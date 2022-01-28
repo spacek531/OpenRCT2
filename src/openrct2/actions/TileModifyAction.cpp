96a90da32c1cf54370830b7be0bf69c8a6961864
spacek531 <spacek531@comcast.net>
2022-01-28 01:50:44 -0800

    modify TileModifyAction

--- src/openrct2/actions/TileModifyAction.cpp
+++ src/openrct2/actions/TileModifyAction.cpp
@@ -177,6 +177,7 @@ GameActions::Result TileModifyAction::QueryExecute(bool isExecuting) const
         }
         case TileModifyType::TrackSetBlockBrake:
         {
+            // TODO: find 0th sequence and set
             const auto elementIndex = _value1;
             const bool blockBrake = _value2;
             res = TileInspector::TrackSetBlockBrake(_loc, elementIndex, blockBrake, isExecuting);
