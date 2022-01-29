809df6e73c87ed663643dbfa449f72a2458a0dab
spacek531 <spacek531@comcast.net>
2022-01-29 13:39:51 -0800

    add junior coaster block brake functionality

--- src/openrct2/ride/coaster/JuniorRollerCoaster.cpp
+++ src/openrct2/ride/coaster/JuniorRollerCoaster.cpp
@@ -639,10 +639,10 @@ enum
 
     SPR_JUNIOR_RC_DIAG_BRAKES_E_W = SPR_G2_JUNIOR_DIAG_BRAKES_START,
     SPR_JUNIOR_RC_DIAG_BRAKES_N_S = SPR_G2_JUNIOR_DIAG_BRAKES_START + 1,
-    SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_E_W_OPEN = SPR_G2_JUNIOR_DIAG_BRAKES_START + 2,
-    SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_E_W_CLOSED = SPR_G2_JUNIOR_DIAG_BRAKES_START + 3,
-    SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_N_S_OPEN = SPR_G2_JUNIOR_DIAG_BRAKES_START + 4,
-    SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_N_S_CLOSED = SPR_G2_JUNIOR_DIAG_BRAKES_START + 5
+    SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_E_W_CLOSED = SPR_G2_JUNIOR_DIAG_BRAKES_START + 2,
+    SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_E_W_OPEN = SPR_G2_JUNIOR_DIAG_BRAKES_START + 3,
+    SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_N_S_CLOSED = SPR_G2_JUNIOR_DIAG_BRAKES_START + 4,
+    SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_N_S_OPEN = SPR_G2_JUNIOR_DIAG_BRAKES_START + 5
 };
 
 static constexpr const uint32_t junior_rc_track_pieces_flat[3][4] = {
@@ -1855,6 +1855,21 @@ static constexpr const uint32_t junior_rc_track_pieces_diag_brakes[4] = {
     SPR_JUNIOR_RC_DIAG_BRAKES_N_S,
     SPR_JUNIOR_RC_DIAG_BRAKES_E_W,
     SPR_JUNIOR_RC_DIAG_BRAKES_N_S,
+};
+
+static constexpr const uint32_t junior_rc_track_pieces_diag_blockbrakes[2][4] = {
+    {
+        SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_E_W_OPEN,
+        SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_N_S_OPEN,
+        SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_E_W_OPEN,
+        SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_N_S_OPEN,
+    },
+    {
+        SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_E_W_CLOSED,
+        SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_N_S_CLOSED,
+        SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_E_W_CLOSED,
+        SPR_JUNIOR_RC_DIAG_BLOCKBRAKES_N_S_CLOSED,
+    },
 };
 
 void junior_rc_paint_track_flat(
@@ -4464,8 +4479,29 @@ void junior_rc_paint_track_diag_brakes(
     const TrackElement& trackElement)
 {
     track_paint_util_diag_tiles_paint(
+        session, 1, height, direction, trackSequence, session.TrackColours[SCHEME_TRACK], junior_rc_track_pieces_diag_brakes,
+        defaultDiagTileOffsets, defaultDiagBoundLengths, nullptr);
+
+    if (trackSequence == 3)
+    {
+        metal_a_supports_paint_setup(
+            session, (direction & 1) ? METAL_SUPPORTS_FORK_ALT : METAL_SUPPORTS_FORK, junior_rc_diag_support_segment[direction],
+            0, height, session.TrackColours[SCHEME_SUPPORTS]);
+    }
+
+    int32_t blockedSegments = junior_rc_diag_blocked_segments[trackSequence];
+    paint_util_set_segment_support_height(session, paint_util_rotate_segments(blockedSegments, direction), 0xFFFF, 0);
+    paint_util_set_general_support_height(session, height + 32, 0x20);
+}
+
+void junior_rc_paint_track_diag_blockbrakes(
+    paint_session& session, const Ride& ride, uint8_t trackSequence, uint8_t direction, int32_t height,
+    const TrackElement& trackElement)
+{
+    track_paint_util_diag_tiles_paint(
         session, 1, height, direction, trackSequence, session.TrackColours[SCHEME_TRACK],
-        junior_rc_track_pieces_diag_brakes, defaultDiagTileOffsets, defaultDiagBoundLengths, nullptr);
+        junior_rc_track_pieces_diag_blockbrakes[trackElement.GetBrakeClosed()], defaultDiagTileOffsets, defaultDiagBoundLengths,
+        nullptr);
 
     if (trackSequence == 3)
     {
@@ -6202,6 +6238,8 @@ TRACK_PAINT_FUNCTION get_track_paint_function_junior_rc(int32_t trackType)
             return junior_rc_track_on_ride_photo;
         case TrackElemType::DiagBrakes:
             return junior_rc_paint_track_diag_brakes;
+        case TrackElemType::DiagBlockBrakes:
+            return junior_rc_paint_track_diag_blockbrakes;
     }
     return nullptr;
 }
