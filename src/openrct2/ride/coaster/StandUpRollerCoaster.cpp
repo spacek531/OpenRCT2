73841cc96b6731fd8e91223cb8463cdce5a23641
spacek531 <spacek531@comcast.net>
2022-02-12 13:14:43 -0800

    add standup brakes

--- src/openrct2/ride/coaster/StandUpRollerCoaster.cpp
+++ src/openrct2/ride/coaster/StandUpRollerCoaster.cpp
@@ -30,6 +30,28 @@ static constexpr const uint32_t _StandUpBlockBrakeImages[NumOrthogonalDirections
     { STAND_UP_BLOCK_BRAKE_NW_SE_OPEN, STAND_UP_BLOCK_BRAKE_NW_SE_CLOSED },
 };
 
+static constexpr const uint32_t StandupRCDiagBrakeImages[NumOrthogonalDirections] = {
+    SPR_G2_STANDUP_DIAG_BRAKES,
+    SPR_G2_STANDUP_DIAG_BRAKES + 1,
+    SPR_G2_STANDUP_DIAG_BRAKES,
+    SPR_G2_STANDUP_DIAG_BRAKES + 1,
+};
+
+static constexpr const uint32_t StandupRCDiagBlockBrakeImages[2][NumOrthogonalDirections] = {
+    {
+        SPR_G2_STANDUP_DIAG_BRAKES + 3,
+        SPR_G2_STANDUP_DIAG_BRAKES + 5,
+        SPR_G2_STANDUP_DIAG_BRAKES + 3,
+        SPR_G2_STANDUP_DIAG_BRAKES + 5,
+    },
+    {
+        SPR_G2_STANDUP_DIAG_BRAKES + 2,
+        SPR_G2_STANDUP_DIAG_BRAKES + 4,
+        SPR_G2_STANDUP_DIAG_BRAKES + 2,
+        SPR_G2_STANDUP_DIAG_BRAKES + 4,
+    },
+};
+
 /** rct2: 0x008A7114 */
 static void stand_up_rc_track_flat(
     paint_session& session, const Ride& ride, uint8_t trackSequence, uint8_t direction, int32_t height,
@@ -5462,6 +5484,45 @@ static void stand_up_rc_track_diag_flat(
     }
 }
 
+void stand_up_rc_track_diag_brakes(
+    paint_session& session, const Ride& ride, uint8_t trackSequence, uint8_t direction, int32_t height,
+    const TrackElement& trackElement)
+{
+    track_paint_util_diag_tiles_paint(
+        session, 3, height, direction, trackSequence, session.TrackColours[SCHEME_TRACK], StandupRCDiagBrakeImages,
+        defaultDiagTileOffsets, defaultDiagBoundLengths, nullptr);
+
+    if (trackSequence == 3)
+    {
+        metal_a_supports_paint_setup(
+            session, METAL_SUPPORTS_TUBES, DiagSupportSegments[direction], 8, height, session.TrackColours[SCHEME_SUPPORTS]);
+    }
+
+    int32_t blockedSegments = DiagBlockedSegments[trackSequence];
+    paint_util_set_segment_support_height(session, paint_util_rotate_segments(blockedSegments, direction), 0xFFFF, 0);
+    paint_util_set_general_support_height(session, height + 32, 0x20);
+}
+
+void stand_up_rc_track_diag_block_brakes(
+    paint_session& session, const Ride& ride, uint8_t trackSequence, uint8_t direction, int32_t height,
+    const TrackElement& trackElement)
+{
+    track_paint_util_diag_tiles_paint(
+        session, 3, height, direction, trackSequence, session.TrackColours[SCHEME_TRACK],
+        StandupRCDiagBlockBrakeImages[trackElement.GetBrakeClosed()], defaultDiagTileOffsets, defaultDiagBoundLengths,
+        nullptr);
+
+    if (trackSequence == 3)
+    {
+        metal_a_supports_paint_setup(
+            session, METAL_SUPPORTS_TUBES, DiagSupportSegments[direction], 8, height, session.TrackColours[SCHEME_SUPPORTS]);
+    }
+
+    int32_t blockedSegments = DiagBlockedSegments[trackSequence];
+    paint_util_set_segment_support_height(session, paint_util_rotate_segments(blockedSegments, direction), 0xFFFF, 0);
+    paint_util_set_general_support_height(session, height + 32, 0x20);
+}
+
 /** rct2: 0x008A7524 */
 static void stand_up_rc_track_diag_25_deg_up(
     paint_session& session, const Ride& ride, uint8_t trackSequence, uint8_t direction, int32_t height,
@@ -8534,6 +8595,10 @@ TRACK_PAINT_FUNCTION get_track_paint_function_stand_up_rc(int32_t trackType)
             return stand_up_rc_track_diag_right_bank;
         case TrackElemType::BlockBrakes:
             return stand_up_rc_track_block_brakes;
+        case TrackElemType::DiagBrakes:
+            return stand_up_rc_track_diag_brakes;
+        case TrackElemType::DiagBlockBrakes:
+            return stand_up_rc_track_diag_block_brakes;
     }
     return nullptr;
 }
