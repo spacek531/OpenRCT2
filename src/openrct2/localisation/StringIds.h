478ee4988661ea2d789fccd29b6ef35e4b5898e2
Spacek531 <spacek531@comcast.net>
2022-02-19 14:32:56 -0800

    unify brake and booster speeds
    only allow td6-compatible increments
    
    fix formatting
    
    add missing preprocessor directive

--- src/openrct2/localisation/StringIds.h
+++ src/openrct2/localisation/StringIds.h
@@ -3999,6 +3999,8 @@ enum : uint16_t
 
     STR_CANT_FIRE_STAFF_FIXING = 6591,
     STR_CANT_FIRE_STAFF_INSPECTING = 6592,
+    STR_SPEED_TOO_HIGH = 6593,
+    STR_TRACK_DESIGN_SPEED_UNSERIALIZABLE = 6594,
 
     // Have to include resource strings (from scenarios and objects) for the time being now that language is partially working
     /* MAX_STR_COUNT = 32768 */ // MAX_STR_COUNT - upper limit for number of strings, not the current count strings
