ff08ac3ce56a7b29ceaf6d6024dd7c2d4dca1cfc
Spacek531 <spacek531@comcast.net>
2022-11-28 12:52:09 -0800

    fix multidim-td6 interaction

--- src/openrct2/ride/Vehicle.cpp
+++ src/openrct2/ride/Vehicle.cpp
@@ -7545,7 +7545,7 @@ bool Vehicle::UpdateTrackMotionForwardsGetNewTrack(uint16_t trackType, Ride* cur
     }
 
     trackType = tileElement->AsTrack()->GetTrackType();
-    if (trackType != TrackElemType::Brakes && trackType != TrackElemType::BlockBrakes)
+    if (trackType != TrackElemType::Brakes)
     {
         target_seat_rotation = tileElement->AsTrack()->GetSeatRotation();
     }
@@ -7952,7 +7952,7 @@ bool Vehicle::UpdateTrackMotionBackwardsGetNewTrack(uint16_t trackType, Ride* cu
     }
 
     trackType = tileElement->AsTrack()->GetTrackType();
-    if (trackType != TrackElemType::Brakes && trackType != TrackElemType::BlockBrakes)
+    if (trackType != TrackElemType::Brakes)
     {
         target_seat_rotation = tileElement->AsTrack()->GetSeatRotation();
     }
