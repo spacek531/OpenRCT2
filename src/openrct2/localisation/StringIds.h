64899e7dc8a21c45d76526a8c8c125a5b0c65288
Spacek531 <spacek531@comcast.net>
2022-12-05 22:26:40 -0800

    add string for future use

--- src/openrct2/localisation/StringIds.h
+++ src/openrct2/localisation/StringIds.h
@@ -3929,6 +3929,7 @@ enum : uint16_t
     STR_TILE_INSPECTOR_TRACK_BRAKE = 6536,
     STR_RIDE_CONSTRUCTION_BLOCK_BRAKE_SPEED = 6537,
     STR_RIDE_CONSTRUCTION_BLOCK_BRAKE_SPEED_LIMIT_TIP = 6538,
+    STR_TRACK_DESIGN_BLOCK_BRAKE_SPEED_RESET = 6539,
 
     // Have to include resource strings (from scenarios and objects) for the time being now that language is partially working
     /* MAX_STR_COUNT = 32768 */ // MAX_STR_COUNT - upper limit for number of strings, not the current count strings
