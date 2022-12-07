b7438c0d9bb50c5797f1e1fe20b9528b8aa12e67
Spacek531 <spacek531@comcast.net>
2022-12-06 23:31:14 -0800

    remove modifications in import code

--- src/openrct2/rct2/S6Importer.cpp
+++ src/openrct2/rct2/S6Importer.cpp
@@ -1346,6 +1346,7 @@ namespace RCT2
                     auto src2 = src->AsSurface();
 
                     dst2->SetSlope(src2->GetSlope());
+
                     dst2->SetSurfaceStyle(src2->GetSurfaceStyle());
                     dst2->SetEdgeStyle(src2->GetEdgeStyle());
 
@@ -1416,8 +1417,6 @@ namespace RCT2
                     dst2->SetBrakeClosed(src2->GetBrakeClosed() || (trackType == TrackElemType::Brakes));
                     dst2->SetIsIndestructible(src2->IsIndestructible());
                     // Skipping IsHighlighted()
-                    if (trackType == TrackElemType::Brakes)
-                        dst2->SetBrakeClosed(true);
 
                     // Import block brakes to keep legacy behaviour
                     if (trackType == TrackElemType::BlockBrakes)
