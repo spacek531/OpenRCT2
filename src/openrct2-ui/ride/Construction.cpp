eead9bbbd2f4ff96f9078884b632ba9ab084fa34
Spacek531 <spacek531@comcast.net>
2023-04-15 21:48:11 -0700

    implement SpecialElementsList

--- src/openrct2-ui/ride/Construction.cpp
+++ src/openrct2-ui/ride/Construction.cpp
@@ -12,7 +12,11 @@
 #include <openrct2/actions/RideCreateAction.h>
 #include <openrct2/ride/Ride.h>
 #include <openrct2/ride/RideConstruction.h>
+#include <openrct2/ride/RideData.h>
 #include <openrct2/ride/RideTypes.h>
+#include <openrct2/ride/TrackData.h>
+
+using namespace OpenRCT2::TrackMetaData;
 
 /**
  *
@@ -35,4 +39,94 @@ void RideConstructNew(RideSelection listItem)
     });
 
     GameActions::Execute(&gameAction);
+}
+
+SpecialElementsList BuildSpecialElementsList(
+    const Ride& currentRide, uint8_t buildDirection, uint8_t buildSlope, uint8_t buildBank, RideConstructionState state)
+{
+    auto buildDirectionIsDiagonal = TrackPieceDirectionIsDiagonal(buildDirection);
+    SpecialElementsList list;
+
+    for (track_type_t trackType : DropdownOrder)
+    {
+        const auto& ted = GetTrackElementDescriptor(trackType);
+        if (!IsTrackEnabled(ted.Definition.type))
+            continue;
+        int32_t trackSlope, trackBank;
+        bool trackDirectionIsDiagonal = false;
+
+        if (state == RideConstructionState::Front || state == RideConstructionState::Place)
+        {
+            trackSlope = ted.Definition.vangle_start;
+            trackBank = ted.Definition.bank_start;
+            trackDirectionIsDiagonal = TrackPieceDirectionIsDiagonal(ted.Coordinates.rotation_begin);
+        }
+        else if (state == RideConstructionState::Back)
+        {
+            trackSlope = ted.Definition.vangle_end;
+            trackBank = ted.Definition.bank_end;
+            trackDirectionIsDiagonal = TrackPieceDirectionIsDiagonal(ted.Coordinates.rotation_end);
+        }
+        else
+        {
+            // if it's building forwards nor backwards, no list is available
+            return list;
+        }
+
+        // Only show up half helixes on alpine coaster
+        if (currentRide.GetRideTypeDescriptor().HasFlag(RIDE_TYPE_FLAG_UP_INCLINE_REQUIRES_LIFT)
+            && !gCheatsEnableAllDrawableTrackPieces
+            && ((
+                trackType == TrackElemType::LeftHalfBankedHelixUpSmall
+                || trackType == TrackElemType::RightHalfBankedHelixUpSmall
+                || trackType == TrackElemType::LeftHalfBankedHelixUpLarge
+                || trackType == TrackElemType::RightHalfBankedHelixUpLarge)))
+            continue;
+
+        // At this point, the piece is desired to be in the list. Check if it should be disabled first and attempt to
+        // collate entries.
+
+        bool entryIsDisabled = true;
+
+        // If all 3 orientations match, the track can be built
+        if (buildDirectionIsDiagonal == trackDirectionIsDiagonal && buildSlope == trackSlope && buildBank == trackBank)
+            entryIsDisabled = false;
+
+        // Additional tower bases can only be built if the ride allows for it (elevator)
+        if (trackType == TrackElemType::TowerBase
+            && !currentRide.GetRideTypeDescriptor().HasFlag(RIDE_TYPE_FLAG_ALLOW_EXTRA_TOWER_BASES))
+            entryIsDisabled = true;
+
+        SpecialElement* previousEntryOfSameName = nullptr;
+        // Search for any duplicate names in the last 10 entries. If a duplicate exists and is disabled, replace that entry with
+        // this entry
+        for (int32_t searchIndex = list.NumElements - 1; searchIndex >= 0 && searchIndex > list.NumElements - 11; searchIndex--)
+        {
+            if (GetTrackElementDescriptor(list.Elements[searchIndex].TrackType).Description == ted.Description)
+            {
+                previousEntryOfSameName = &list.Elements[searchIndex];
+                break;
+            }
+        }
+
+        if (previousEntryOfSameName != nullptr)
+        {
+            // If the current element is disabled, do not add current element.
+            if (entryIsDisabled)
+                continue;
+            // If the previous element is disabled and current element is enabled, replace the previous element
+            if (previousEntryOfSameName->Disabled && !entryIsDisabled)
+            {
+                previousEntryOfSameName->TrackType = trackType;
+                previousEntryOfSameName->Disabled = false;
+                list.HasActiveElements = true;
+                continue;
+            }
+            // If the previous element and current element are enabled, add both to the list
+        }
+        list.Elements[list.NumElements].TrackType = trackType;
+        list.Elements[list.NumElements++].Disabled = entryIsDisabled;
+        list.HasActiveElements |= !entryIsDisabled;
+    }
+    return list;
 }
--- src/openrct2-ui/ride/Construction.h
+++ src/openrct2-ui/ride/Construction.h
@@ -10,5 +10,153 @@
 #pragma once
 
 #include <openrct2/ride/RideTypes.h>
+#include <openrct2/ride/Track.h>
+
+enum class RideConstructionState : uint8_t;
+
+/**
+ * Order of special track elements dropdown
+ */
+constexpr std::array DropdownOrder = {
+    TrackElemType::EndStation,
+    TrackElemType::SBendLeft,
+    TrackElemType::SBendRight,
+    TrackElemType::LeftVerticalLoop,
+    TrackElemType::RightVerticalLoop,
+    TrackElemType::LeftTwistDownToUp,
+    TrackElemType::RightTwistDownToUp,
+    TrackElemType::LeftTwistUpToDown,
+    TrackElemType::RightTwistUpToDown,
+    TrackElemType::HalfLoopUp,
+    TrackElemType::HalfLoopDown,
+    TrackElemType::LeftCorkscrewUp,
+    TrackElemType::RightCorkscrewUp,
+    TrackElemType::LeftCorkscrewDown,
+    TrackElemType::RightCorkscrewDown,
+    TrackElemType::TowerBase,
+    TrackElemType::TowerSection,
+    TrackElemType::LeftHalfBankedHelixUpSmall,
+    TrackElemType::RightHalfBankedHelixUpSmall,
+    TrackElemType::LeftHalfBankedHelixDownSmall,
+    TrackElemType::RightHalfBankedHelixDownSmall,
+    TrackElemType::LeftHalfBankedHelixUpLarge,
+    TrackElemType::RightHalfBankedHelixUpLarge,
+    TrackElemType::LeftHalfBankedHelixDownLarge,
+    TrackElemType::RightHalfBankedHelixDownLarge,
+    TrackElemType::Booster,
+    TrackElemType::Brakes,
+    TrackElemType::LeftQuarterBankedHelixLargeUp,
+    TrackElemType::RightQuarterBankedHelixLargeUp,
+    TrackElemType::LeftQuarterBankedHelixLargeDown,
+    TrackElemType::RightQuarterBankedHelixLargeDown,
+    TrackElemType::LeftQuarterHelixLargeUp,
+    TrackElemType::RightQuarterHelixLargeUp,
+    TrackElemType::LeftQuarterHelixLargeDown,
+    TrackElemType::RightQuarterHelixLargeDown,
+    TrackElemType::Waterfall,
+    TrackElemType::Rapids,
+    TrackElemType::Watersplash,
+    TrackElemType::Whirlpool,
+    TrackElemType::CableLiftHill,
+    TrackElemType::ReverseFreefallSlope,
+    TrackElemType::ReverseFreefallVertical,
+    TrackElemType::BrakeForDrop,
+    TrackElemType::LogFlumeReverser,
+    TrackElemType::SpinningTunnel,
+    TrackElemType::LeftBarrelRollUpToDown,
+    TrackElemType::RightBarrelRollUpToDown,
+    TrackElemType::LeftBarrelRollDownToUp,
+    TrackElemType::RightBarrelRollDownToUp,
+    TrackElemType::PoweredLift,
+    TrackElemType::LeftLargeHalfLoopUp,
+    TrackElemType::RightLargeHalfLoopUp,
+    TrackElemType::LeftLargeHalfLoopDown,
+    TrackElemType::RightLargeHalfLoopDown,
+    TrackElemType::LeftFlyerTwistUp,
+    TrackElemType::RightFlyerTwistUp,
+    TrackElemType::LeftFlyerTwistDown,
+    TrackElemType::RightFlyerTwistDown,
+    TrackElemType::FlyerHalfLoopUninvertedUp,
+    TrackElemType::FlyerHalfLoopInvertedDown,
+    TrackElemType::LeftFlyerCorkscrewUp,
+    TrackElemType::RightFlyerCorkscrewUp,
+    TrackElemType::LeftFlyerCorkscrewDown,
+    TrackElemType::RightFlyerCorkscrewDown,
+    TrackElemType::HeartLineTransferUp,
+    TrackElemType::HeartLineTransferDown,
+    TrackElemType::LeftHeartLineRoll,
+    TrackElemType::RightHeartLineRoll,
+    TrackElemType::MinigolfHoleA,
+    TrackElemType::MinigolfHoleB,
+    TrackElemType::MinigolfHoleC,
+    TrackElemType::MinigolfHoleD,
+    TrackElemType::MinigolfHoleE,
+    TrackElemType::MultiDimInvertedFlatToDown90QuarterLoop,
+    TrackElemType::Up90ToInvertedFlatQuarterLoop,
+    TrackElemType::InvertedFlatToDown90QuarterLoop,
+    TrackElemType::LeftCurvedLiftHill,
+    TrackElemType::RightCurvedLiftHill,
+    TrackElemType::LeftReverser,
+    TrackElemType::RightReverser,
+    TrackElemType::AirThrustTopCap,
+    TrackElemType::AirThrustVerticalDown,
+    TrackElemType::AirThrustVerticalDownToLevel,
+    TrackElemType::BlockBrakes,
+    TrackElemType::MultiDimUp90ToInvertedFlatQuarterLoop,
+    TrackElemType::MultiDimFlatToDown90QuarterLoop,
+    TrackElemType::MultiDimInvertedUp90ToFlatQuarterLoop,
+    TrackElemType::RotationControlToggle,
+    TrackElemType::LeftLargeCorkscrewUp,
+    TrackElemType::RightLargeCorkscrewUp,
+    TrackElemType::LeftLargeCorkscrewDown,
+    TrackElemType::RightLargeCorkscrewDown,
+    TrackElemType::LeftMediumHalfLoopUp,
+    TrackElemType::RightMediumHalfLoopUp,
+    TrackElemType::LeftMediumHalfLoopDown,
+    TrackElemType::RightMediumHalfLoopDown,
+    TrackElemType::LeftZeroGRollUp,
+    TrackElemType::RightZeroGRollUp,
+    TrackElemType::LeftZeroGRollDown,
+    TrackElemType::RightZeroGRollDown,
+    TrackElemType::LeftLargeZeroGRollUp,
+    TrackElemType::RightLargeZeroGRollUp,
+    TrackElemType::LeftLargeZeroGRollDown,
+    TrackElemType::RightLargeZeroGRollDown,
+    TrackElemType::LeftFlyerLargeHalfLoopUninvertedUp,
+    TrackElemType::RightFlyerLargeHalfLoopUninvertedUp,
+    TrackElemType::LeftFlyerLargeHalfLoopInvertedDown,
+    TrackElemType::RightFlyerLargeHalfLoopInvertedDown,
+    TrackElemType::LeftFlyerLargeHalfLoopInvertedUp,
+    TrackElemType::RightFlyerLargeHalfLoopInvertedUp,
+    TrackElemType::LeftFlyerLargeHalfLoopUninvertedDown,
+    TrackElemType::RightFlyerLargeHalfLoopUninvertedDown,
+    TrackElemType::FlyerHalfLoopInvertedUp,
+    TrackElemType::FlyerHalfLoopUninvertedDown,
+};
+constexpr const size_t DropdownLength = DropdownOrder.size();
+
+// Reminder to add your track element to the list, if applicable
+static_assert(TrackElemType::Count == 337);
+
+constexpr bool TrackPieceDirectionIsDiagonal(const uint8_t direction)
+{
+    return direction >= NumOrthogonalDirections;
+}
+
+struct SpecialElement
+{
+    track_type_t TrackType;
+    bool Disabled;
+};
+
+struct SpecialElementsList
+{
+    int32_t NumElements{};
+    bool HasActiveElements = false;
+    std::array<SpecialElement, DropdownLength> Elements{};
+};
+
+SpecialElementsList BuildSpecialElementsList(
+    const Ride& currentRide, uint8_t buildDirection, uint8_t buildSlope, uint8_t buildBank, RideConstructionState state);
 
 void RideConstructNew(RideSelection listItem);
