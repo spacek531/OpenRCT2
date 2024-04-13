a2606c67789edc1473c45c6d34de2a4ff34abbe3
Spacek531 <spacek531@comcast.net>
2024-04-13 00:02:51 -0700

    add localisation

--- src/openrct2/localisation/StringIds.h
+++ src/openrct2/localisation/StringIds.h
@@ -4043,6 +4043,12 @@ enum : uint16_t
 
     STR_ERR_INVALID_COLOUR = 6625,
 
+    STR_SPEED_TOO_HIGH = 6626,
+    STR_TRACK_DESIGN_SPEED_UNSERIALIZABLE = 6627,
+    STR_RIDE_SET_VEHICLE_LEGACY_SPEED_FAIL = 6628,
+    STR_OPTION_USE_LEGACY_SPEED = 6629,
+    STR_OPTION_USE_LEGACY_SPEED_TIP = 6630,
+
     // Have to include resource strings (from scenarios and objects) for the time being now that language is partially working
     /* MAX_STR_COUNT = 32768 */ // MAX_STR_COUNT - upper limit for number of strings, not the current count strings
 };
